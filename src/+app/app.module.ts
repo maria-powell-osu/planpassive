4b4953aac4eed03fa4a6c757d22e8f378cea9b15
Maria <maria.powell.osu.com>
2017-07-04 12:39:02 -0700

    hpme page basics

--- src/+app/app.module.ts
+++ src/+app/app.module.ts
@@ -6,13 +6,15 @@ import { AboutModule } from './+about/about.module';
 import { TodoModule } from './+todo/todo.module';
 
 import { SharedModule } from './shared/shared.module';
+import { NavbarComponent } from "./shared/navbar/navbar.component";
+import { FooterComponent } from './shared/footer/footer.component'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent, XLargeDirective } from './app.component';
 
 
 @NgModule({
-  declarations: [ AppComponent, XLargeDirective ],
+  declarations: [ AppComponent, XLargeDirective, NavbarComponent, FooterComponent ],
   imports: [
     SharedModule,
     HomeModule,
